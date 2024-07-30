e2adc2d087af4582a702a8e3fc18aae4aeca1301
srilekha <srilekha.karnala@zemosolabs.com>
2024-07-30 17:08:10 +0530

    Fix Error

--- Seq.java
+++ Seq.java
@@ -1,8 +1,9 @@
+
 /*
 Alice and Bob are playing a game on a sequence a1,a2,...,an of length n . They move in turns and Alice moves first.  In the turn of each player, he or she should select an integer and remove it from the sequence. The game ends when there is no integer left in the sequence.  Alice wins if the sum of her selected integers is even; otherwise, Bob wins.  Your task is to determine who will win the game, if both players play optimally.
 */
 import java.util.*;
-public class Game {
+public class Seq{
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
@@ -22,6 +23,7 @@ public class Game {
     
     }
 }
+
 /*   Algorithm
 
 * Read integer n (length of sequence) 
